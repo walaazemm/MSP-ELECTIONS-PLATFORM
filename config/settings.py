e671a56a8df9a035add31ea9fd1eeff11c734539
WALAEDDINE ZEMMAMOUCHE <124275224+walaazemm@users.noreply.github.com>
2026-06-29 14:19:39 +0100

    adding static root files

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ⚠️ CRITICAL: Tell Django to use our Custom User Model
 AUTH_USER_MODEL = 'accounts.CustomUser'
